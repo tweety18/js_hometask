d6cb9be5b03220e07c5a4e161409de25af6ac5e7
Alona <alyonka159@gmail.com>
2020-03-23 19:01:45 +0200

    added new homatask files

---
+++ app1.js
@@ -0,0 +1,38 @@
+// Task 1
+// let arr = ["Яблоко", "Апельсин", "Груша"];
+
+// let b = arr[arr.length-1];
+// console.log(b);
+
+// Task 2
+// let arr = [10, 20, 30, 40, 50];
+// let sum = 0;
+
+// 	for (let i = 0; i < arr.length; i++) {
+//  		 sum += arr[i];
+// }
+
+// console.log(sum);
+
+
+// Task 3
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	if(arr[i] < arr[0]){
+//         arr[0] = arr[i];
+//     }
+// };
+
+// console.log(arr[0]);
+
+// Task 4
+// let arr = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	if(arr[i] > arr[0]){
+//         arr[0] = arr[i];
+//     }
+// };
+
+// console.log(arr[0]);
